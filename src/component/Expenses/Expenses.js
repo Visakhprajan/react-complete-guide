97aa0c0f3768a656adb9434024c15782a9c71627
Visakhprajan <visakhprajan@gmail.com>
2021-06-18 14:53:01 +0530

    first module completed

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem 
         title={props.items[0].title} 
         amount={props.items[0].amount} 
@@ -17,7 +18,7 @@ const Expenses = (props) => {
       </ExpenseItem>
       <ExpenseItem 
         title={props.items[2].title} 
-        amount={props.items[2].amount} 
+        amount={props.items[2].amount}  
         date={props.items[2].date}>
       </ExpenseItem>
       <ExpenseItem 
@@ -25,7 +26,7 @@ const Expenses = (props) => {
         amount={props.items[3].amount} 
         date={props.items[3].date}> 
       </ExpenseItem>
-    </div>
+    </Card>
   )
 }
 

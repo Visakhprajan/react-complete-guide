68dc83400c4b02a9ddcb8c36cb5b6ab034b1904b
Visakhprajan <visakhprajan@gmail.com>
2021-06-19 00:14:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React, {useState} from 'react';
 import Expenses from './component/Expenses/Expenses';
 import NewExpense from './component/Expenses/NewExpense/NewExpense';
 
-function App() {
-    const expenses = [
+  const DUMMY_EXPENSES = [
     { id: 'e1', title: 'Toilet paper', amount: 264.67, date: new Date(2021, 6, 17) },
     { id: 'e2', title: 'New Tv', amount: 264.67, date: new Date(2021, 6, 17) },
     { id: 'e3', title: 'Car Insurance', amount: 264.67, date: new Date(2021, 6, 17) },
     { id: 'e4', title: 'New desk(wooden)', amount: 264.67, date: new Date(2021, 6, 17) }
   ];
 
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return (
+        [expense, ...prevExpenses]
+      )
+    })
   }
   return (
     <div className="App">
--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './NewExpense/ExpensesFilter';
 
@@ -12,32 +12,21 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  return (
+
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  
+
+  return ( 
     <Fragment>
       <Card className="expenses">
+        
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler} />
-        <ExpenseItem 
-          title={props.items[0].title} 
-          amount={props.items[0].amount} 
-          date={props.items[0].date}> 
-        </ExpenseItem>
-        <ExpenseItem 
-          title={props.items[1].title} 
-          amount={props.items[1].amount} 
-          date={props.items[1].date}> 
-        </ExpenseItem>
-        <ExpenseItem 
-          title={props.items[2].title} 
-          amount={props.items[2].amount}  
-          date={props.items[2].date}>
-        </ExpenseItem>
-        <ExpenseItem 
-          title={props.items[3].title} 
-          amount={props.items[3].amount} 
-          date={props.items[3].date}> 
-        </ExpenseItem>
+        <ExpensesList items={filterExpenses} />
       </Card>
     </Fragment>
   )
